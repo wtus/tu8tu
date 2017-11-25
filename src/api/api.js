06d88b18f43120b01d2fcb7147d68f6491483899
whthfd <padxianguo@tom.com>
2017-11-25 17:11:17 +0800

    # 完善接口 抽出公共参数，提出 data

--- src/api/api.js
+++ src/api/api.js
@@ -8,22 +8,11 @@ export default {
       url: '/api',
       method: 'get',
       params: {
-        "action": "eggV_5_0",
-        "apkPackageName": "com.to8to.housekee",
-        "style": "13",
-        "appid": "15",
+        action: "eggV_5_0",
         "cityName": "玉溪",
         "to8to_token": "",
-        "systemversion": "21",
-        "imei": "869677028151723",
-        "appversion": "5.3.0",
-        "appostype": "1",
-        "version": "2.5",
         "uid": "",
         "gcjd": "1",
-        "house_type": "2",
-        "channel": "to8toapp",
-        "model": "ad"
       }
     })
   }
--- src/api/fetch.js
+++ src/api/fetch.js
@@ -5,12 +5,25 @@ export function fetch(options) {
     const instance = axios.create({
       headers: {},
       options: {},
+      params:{
+        "apkPackageName": "com.to8to.housekee",
+        "imei": "869677028151723",
+        "systemversion": "21",
+        "appversion": "5.3.0",
+        "appostype": "1",
+        "version": "2.5",
+        "channel": "to8toapp",
+        "style": "13",
+        "appid": "15",
+        "house_type": "2",
+        "model": "ad"
+      },
       timeout: 10 * 1000
     });
 
     instance(options)
       .then(res => {
-          resolve(res)
+          resolve(res.data)
         }
       ).catch(err => {
       reject(err)
