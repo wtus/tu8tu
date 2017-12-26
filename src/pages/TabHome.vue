d8d0a66b2092e38d70ee9e83248881d050b62329
wuht <wuht@jointem.com>
2017-12-26 11:30:56 +0800

    log

--- src/pages/TabHome.vue
+++ src/pages/TabHome.vue
@@ -1,5 +1,6 @@
 <style lang="stylus" type="text/stylus">
   .TabHome-component {
+    background-color #eee
   }
 
   slide
@@ -9,10 +10,23 @@
     height 100%
     width 100%
 
+  .selectionBar
+    height 200px
+    background-color white
+  .cardList
+    height 420px
+    .col1
+      width 489px
+    .col2
+      width 476px
+      img
+        height 193px
+
 </style>
 
 <template>
   <div class="TabHome-component">
+    <!--轮播图-->
     <slide>
       <div v-for="item in carouselList">
         <a :href="item.linkurl">
@@ -20,6 +34,23 @@
         </a>
       </div>
     </slide>
+    <!---->
+    <div class="selectionBar flex fjc-around fai-center">
+      <div>s</div>
+      <div>s</div>
+      <div>s</div>
+      <div>s</div>
+    </div>
+    <!--dddd-->
+    <div class="cardList flex">
+      <div class="col1">
+        <img src="http://pic.to8to.com/attch/day_171010/1720383.gif" alt="">
+      </div>
+      <div class="flex-column clo2">
+        <img src="http://pic.to8to.com/attch/day_171010/1389155.gif" alt="">
+        <img src="http://pic.to8to.com/attch/day_170908/1114570.png" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -38,7 +69,13 @@
       var vue = this
       this.$api.getCarouselData().then(function (data) {
         vue.carouselList = (data.data.bannerInfo)
-        console.log(data.data)
+      }).catch(function (err) {
+        console.log(err)
+      })
+
+      this.$api.getIndexData().then(function (data) {
+        vue.cardList = (data.data.cardList)
+        console.log(vue.cardList)
       }).catch(function (err) {
         console.log(err)
       })
@@ -46,7 +83,8 @@
     computed: {},
     data() {
       return {
-        carouselList: []
+        carouselList: [],
+        cardList: []
       }
     },
     methods: {}
