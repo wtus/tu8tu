151e31867f30ccbddb2ee3689c7aa5590e937bc6
whthfd <padxianguo@tom.com>
2017-12-29 23:42:35 +0800

    # 代码整理一下

--- src/components/water-fall.vue
+++ src/components/water-fall.vue
@@ -60,48 +60,51 @@
     },
     methods: {
       calcPosition() {
-        var imgs = this.$refs.container.children
-        console.log(imgs)
-        var columHeights = new Array(this.column) //用来保存每一列的当前起始高度
-        for (let i = 0; i < this.column; i++) {
+        let column = this.column
+        let columHeights = new Array(column) //用来保存每一列的当前起始高度
+        for (let i = 0; i < column; i++) {
           columHeights[i] = 0
         }
-        this.alpha = NP.divide(1, this.column)
+        this.alpha = NP.divide(1, column)
+
+        let rem = parseFloat(window.getComputedStyle(document.documentElement)["fontSize"].replace("px", ""))
+        let containerWidth = this.$refs.container.clientWidth
+        let space = this.space
+        let imgs = this.$refs.container.children
         for (let i = 0; i < imgs.length; i++) {
-          var rowIndex = i % this.column
-          console.log(rowIndex / this.column * 100)
-          var rem = parseFloat(window.getComputedStyle(document.documentElement)["fontSize"].replace("px", ""))
-          imgs[i].style.position = `absolute`
-          imgs[i].style.width = `${(this.$refs.container.clientWidth) / this.column / rem}rem`
-          if(rowIndex!==0) {
-            console.log(rowIndex / this.column+this.space/108)
-            imgs[i].style.left = `${(rowIndex / this.column+this.space/(1080)) * 100}%`
-          }else {
-            imgs[i].style.left = `0%`
+          let columnIndex = i % column
+          let eleItem = imgs[i]
+
+          eleItem.style.position = `absolute`
+          eleItem.style.width = `${(containerWidth) / column / rem}rem`
+          eleItem.style.top = `${columHeights[columnIndex]}rem`
+          if (columnIndex !== 0) {
+            console.log(columnIndex / column + space / 108)
+            eleItem.style.left = `${(columnIndex / column + space / (1004)) * 100}%`
+          } else {
+            eleItem.style.left = `0%`
           }
-          imgs[i].style.top = `${columHeights[rowIndex]}rem`
-//          this.setElement(imgs[i], columHeights, rowIndex)
-          columHeights[rowIndex] += imgs[i].clientHeight/rem + this.space/108
-          console.log(columHeights)
-          /***
-           * 总结一下
-           * 计算 rem
-           *  1 已经渲染的px （除以）/window.rem
-           *  2 设计稿上的px （除以）/设计稿宽度的10分之一
-           *  3 两个计算出的rem 可以相加减
-           *
-           * */
+          columHeights[columnIndex] += imgs[i].clientHeight / rem + space / 108
         }
+
+        /***
+         * 总结一下
+         * 计算 rem
+         *  1 已经渲染的px （除以）/window.rem
+         *  2 设计稿上的px （除以）/设计稿宽度的10分之一
+         *  3 两个计算出的rem 可以相加减
+         *
+         * */
       },
-      setElement(ele, i, rowIndex) {
+      setElement(ele, i, columnIndex) {
         var imgEle = ele.getElementsByTagName('test')[0]
 
         var alpha = imgEle.height / imgEle.width
-        var alpha2 = this.$refs.container.clientWidth / this.column - this.space/rem
+        var alpha2 = this.$refs.container.clientWidth / this.column - this.space / rem
         ele.style.width = `${alpha2}px`
         ele.style.height = `${alpha * alpha2}px`
-        ele.style.top = `${i[rowIndex]}` + 'px'
-        ele.style.left = `${rowIndex / this.column * 100}%`
+        ele.style.top = `${i[columnIndex]}` + 'px'
+        ele.style.left = `${columnIndex / this.column * 100}%`
       }
     }
   };
